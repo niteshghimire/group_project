4cc5f59f6cd9957a6efc78adb6d3d880a06a6f01
paripradhan <46806838+paripradhan@users.noreply.github.com>
2020-01-29 00:42:48 +0545

    Add files via upload

---
+++ courses.php
@@ -0,0 +1,105 @@
+<?php
+	require('common/stdHead.php');
+?>	
+
+<aside id="fh5co-hero">
+		<div class="flexslider">
+			<ul class="slides">
+		   	<li style="background-image: url(images/project-2.jpg);">
+		   		<div class="overlay-gradient"></div>
+		   		<div class="container">
+		   			<div class="row">
+			   			<div class="col-md-8 col-md-offset-2 text-center slider-text">
+			   				<div class="slider-text-inner">
+			   					<h1>Courses</h1>
+							
+							<li class="crs"><a href="course1.php">CSY2001-NAM09-1920: 19/20 Computer Networks (NAMI) (CSY2001-NAM09)</a></li>
+							<li class="crs"><a href="course2.php">CSY2038-NAM09-1920: 19/20 Databases 2 (NAMI) (CSY2038-NAM09)</a></li>
+							<li class="crs"><a href="course3">CSY2027-NAM09-1920: 19/20 Group Project (NAMI) (CSY2027-NAM09)</a></li>
+							<li class="crs"><a href="course2.php">CSY2026-NAM09-1920: 19/20 Modern Networks (NAMI) (CSY2026-NAM09)</a></li>
+							<li class="crs"><a href="course3">CSY2002-NAM09-1920: 19/20 Operating Systems (NAMI) (CSY2002-NAM09)</a></li>
+							
+			   				</div>
+			   			</div>
+			   		</div>
+		   		</div>
+		   	</li>		   	
+		  	</ul>
+	  	</div>
+	</aside>
+
+<div id="fh5co-blog">
+		<div class="container">
+			<div class="row animate-box">
+				<div class="col-md-8 col-md-offset-2 text-center fh5co-heading">
+					<h2>News and Events</h2>
+					<p>Latest News and Events From NAMI</p>
+				</div>
+			</div>
+			<div class="row row-padded-mb">
+				<div class="col-md-4 animate-box">
+					<div class="fh5co-event">
+						<div class="date text-center"><span>15<br>Mar.</span></div>
+						<h3><a href="#">USA, International Triathlon Event</a></h3>
+						<p>Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the blind texts.</p>
+						<p><a href="#">Read More</a></p>
+					</div>
+				</div>
+				<div class="col-md-4 animate-box">
+					<div class="fh5co-event">
+						<div class="date text-center"><span>15<br>Mar.</span></div>
+						<h3><a href="#">USA, International Triathlon Event</a></h3>
+						<p>Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the blind texts.</p>
+						<p><a href="#">Read More</a></p>
+					</div>
+				</div>
+				<div class="col-md-4 animate-box">
+					<div class="fh5co-event">
+						<div class="date text-center"><span>15<br>Mar.</span></div>
+						<h3><a href="#">New Device Develope by Microsoft</a></h3>
+						<p>Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the blind texts.</p>
+						<p><a href="#">Read More</a></p>
+					</div>
+				</div>
+			</div>
+			<div class="row">
+				<div class="col-lg-4 col-md-4">
+					<div class="fh5co-blog animate-box">
+						<a href="#" class="blog-img-holder" style="background-image: url(images/img_bg_4.jpg);"></a>
+						<div class="blog-text">
+							<h3><a href="#">Healty Lifestyle &amp; Living</a></h3>
+							<span class="posted_on">March. 15th</span>
+							<span class="comment"><a href="">21<i class="icon-speech-bubble"></i></a></span>
+							<p>Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the blind texts.</p>
+						</div> 
+					</div>
+				</div>
+				<div class="col-lg-4 col-md-4">
+					<div class="fh5co-blog animate-box">
+						<a href="#" class="blog-img-holder" style="background-image: url(images/img_bg_4.jpg);"></a>
+						<div class="blog-text">
+							<h3><a href="#">Healty Lifestyle &amp; Living</a></h3>
+							<span class="posted_on">March. 15th</span>
+							<span class="comment"><a href="">21<i class="icon-speech-bubble"></i></a></span>
+							<p>Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the blind texts.</p>
+						</div> 
+					</div>
+				</div>
+				<div class="col-lg-4 col-md-4">
+					<div class="fh5co-blog animate-box">
+						<a href="#" class="blog-img-holder" style="background-image: url(images/img_bg_4.jpg);"></a>
+						<div class="blog-text">
+							<h3><a href="#">Healty Lifestyle &amp; Living</a></h3>
+							<span class="posted_on">March. 15th</span>
+							<span class="comment"><a href="">21<i class="icon-speech-bubble"></i></a></span>
+							<p>Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the blind texts.</p>
+						</div> 
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+
+<?php
+	require('common/footer.php');
+?>	
